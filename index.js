cf969e161d78031ad1dda89bc752696ba41c3e09
Macbook Pro <macbookpro@192.168.0.211>
2025-04-17 20:54:08 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const File = require("./src/read_file")
+
+;(async () => {
+  const filePath = './csv/marco_2025.csv'
+  const users = await File.csvToJSON(filePath)
+
+  users.forEach(user => {
+    const finalizado = user.Finalizado.trim()
+
+    user.Finalizado = (
+      finalizado.includes('CANCELADO') ||
+      finalizado === '' || 
+      /^[\?]+$/.test(finalizado)
+    ) ? false : finalizado
+  })
+
+  const usersWithInvalidNames = users.filter(user => {
+    const nome = user.Nome?.trim()
+    return nome === '' || /^[\?]+$/.test(nome)
+  })
+  .map(user => ({
+    Nome: user.Nome,
+    Whatsapp: user.Whatsapp
+  }))
+
+//   console.log(users)
+  console.log(usersWithInvalidNames.length)
+})()
---
+++ src/read_file.js
@@ -0,0 +1,50 @@
+const { readFile } = require('fs/promises')
+const { error } = require('./constants')
+
+// novos campos que queremos no resultado final
+const FIELDS_TO_PROCESS = ['Finalizado', 'Nome', 'Whatsapp']
+
+class File {
+  static async csvToJSON(filePath) {
+    const content = await readFile(filePath, "utf8")
+    const validation = this.isValid(content)
+    if(!validation.valid) throw new Error(validation.error)
+
+    const result = this.parseCSVToJSON(content)
+    return result
+  }
+
+  static isValid(csvString) {
+    const [header, ...fileWithoutHeader] = csvString.split(/\r?\n/)
+    if (!fileWithoutHeader.length) {
+      return {
+        error: 'O arquivo está vazio ou possui apenas o header',
+        valid: false
+      }
+    }
+    return { valid: true }
+  }
+
+  static parseCSVToJSON(csvString) {
+    const lines = csvString.split(/\r?\n/).filter(Boolean) // remove linhas vazias
+    const header = lines.shift().split(',')
+
+    const indexesToExtract = FIELDS_TO_PROCESS.map(field => header.indexOf(field))
+
+    const users = lines.map(line => {
+      const columns = line.split(',')
+      const user = {}
+
+      FIELDS_TO_PROCESS.forEach((field, i) => {
+        const columnIndex = indexesToExtract[i]
+        user[field] = columns[columnIndex]?.trim() ?? null
+      })
+
+      return user
+    })
+
+    return users
+  }
+}
+
+module.exports = File
